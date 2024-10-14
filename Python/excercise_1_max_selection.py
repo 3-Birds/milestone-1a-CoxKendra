85d63509b016898ec0cd31e72b88060fb6b992c2
CoxKendra <bode.csaba19@gmail.com>
2024-10-14 16:57:39 +0200

    done

--- Python/excercise_1_max_selection.py
+++ Python/excercise_1_max_selection.py
@@ -7,5 +7,36 @@
 # - In case of Empty input string, returns None
 
 def get_max(numbers:list) -> int:
-    pass
+    if numbers is None or len(numbers) == 0:
+        return None
+    result = numbers[0]
+    for i in numbers:
+        if i > result:
+            result = i
+    return result
+
+
+    # TEST OF MAX SELECTION EXCERCISE
+
+import unittest
+from excercise_1_max_selection import get_max
+
+# AUTOMATED TEST, DO NOT MODIFY
+class TestStringMethods(unittest.TestCase):
+    def test_basic_test_cases(self):
+        self.assertEqual(get_max([1,2,3,4,5,6]), 6)
+        self.assertEqual(get_max([1,2,6,9,5,5]), 9)
+        self.assertEqual(get_max([-1,-2,-6,-9,-5,-5]), -1)
+
+    def test_basic_test_cases_with_none(self):
+        self.assertEqual(get_max([]), None)
+        self.assertEqual(get_max(None), None)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+        
+
 
--- SQL/exercisse_3_order_of_customers.sql
+++ SQL/exercisse_3_order_of_customers.sql
@@ -4,6 +4,16 @@ Question 1: Total Orders Per Customer
 Write a query to display the total number of orders placed by each customer, 
 along with their name and the date they joined. Sort the results by the total number of orders in descending order.
 */
+SELECT 
+    c.customer_name, c.join_date, COUNT(o.order_date) as "Total Orders"
+FROM
+    customers c
+JOIN 
+    orders o ON c.customer_id = o.customer_id
+GROUP BY
+    c.customer_id, c.join_date, c.customer_name
+ORDER BY
+    "Total Orders" DESC
 
 
 
@@ -14,3 +24,16 @@ For each customer, calculate their total spending (sum of the total amounts from
 and rank them by total spending within their respective country. 
 Use a **window function** to rank the customers.
 */
+
+SELECT 
+    c.customer_name, SUM(oi.price * oi.quantity) as Spending
+FROM
+    customers c
+JOIN
+    Orders o ON c.customer_id = o.customer_id
+JOIN 
+    OrderItems oi ON o.order_id = oi.order_id
+GROUP BY
+    customer_name
+ORDER BY
+    Spending DESC
